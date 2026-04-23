cfdc302cdd5c276d4470409f16be1103e2b77545
Orval Vera <samuelorval@gmail.com>
2026-04-23 15:54:33 -0600

    refactor(factory): eliminar cadena condicional en OrderFactory (#6 #8)

--- BrewJourney/patterns/factory/OrderFactory.js
+++ BrewJourney/patterns/factory/OrderFactory.js
@@ -2,28 +2,32 @@ import { Order } from '../../domain/Order.js';
 import { Beverage } from '../../domain/Beverage.js';
 import { Dessert } from '../../domain/Dessert.js';
 
+const PRODUCT_BUILDERS = {
+  beverage: (item) => new Beverage(item.name, item.price, item.size),
+  dessert: (item) => new Dessert(item.name, item.price, item.typeCategory)
+};
+
 export class OrderFactory {
   static createOrder(type, user, cafe, itemsData) {
     const order = new Order(`${type}_${Date.now()}`, user, cafe);
-    
+
     if (!itemsData || itemsData.length === 0) {
       return order;
     }
 
-    itemsData.forEach(item => {
-      let product;
-      
-      if (item.type === 'beverage') {
-        product = new Beverage(item.name, item.price, item.size);
-      } else if (item.type === 'dessert') {
-        product = new Dessert(item.name, item.price, item.typeCategory);
-      } else {
-        throw new Error(`Tipo de producto desconocido: ${item.type}`);
-      }
-      
+    itemsData.forEach((item) => {
+      const product = OrderFactory.createProduct(item);
       order.addProduct(product);
     });
 
     return order;
+  }
+
+  static createProduct(item) {
+    const builder = PRODUCT_BUILDERS[item.type];
+    if (!builder) {
+      throw new Error(`Tipo de producto desconocido: ${item.type}`);
+    }
+    return builder(item);
   }
 }
